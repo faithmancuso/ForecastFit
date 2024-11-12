50942968968a564fa97b35ad30bde0a79e257c41
Faith <faithmancuso@Faiths-MBP.lan>
2024-11-12 15:16:26 -0500

    trying to fix form success submission by importing flask_cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 # Main application code to run the server
 
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
