2bcc048be5450a894796b78cdfe5d708c5cd7caf
DiviCSU <d.rajaraman@vikes.csuohio.edu>
2024-12-01 19:13:05 -0500

    temperature changes - succesful

--- app/main.py
+++ app/main.py
@@ -4,13 +4,25 @@ import requests
 app = Flask(__name__)
 WEATHER_API_KEY = "3fc72f97a7404f9a8d0213532241211"
 
-def fetch_weather_data(zip_code, days):
+def fetch_weather_data(zip_code, days, temp_unit='F'):
     """
     Fetch weather data for a specified number of days.
     """
     url = f"http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={zip_code}&days={days}"
     response = requests.get(url)
-    return response.json()
+    data = response.json()
+
+    # Convert temperatures if necessary
+    if temp_unit == 'C':
+        for day in data['forecast']['forecastday']:
+            day['day']['maxtemp_c'] = (day['day']['maxtemp_f'] - 32) * 5 / 9
+            day['day']['mintemp_c'] = (day['day']['mintemp_f'] - 32) * 5 / 9
+            day['day']['avgtemp_c'] = (day['day']['avgtemp_f'] - 32) * 5 / 9
+            for hour in day['hour']:
+                hour['temp_c'] = (hour['temp_f'] - 32) * 5 / 9
+                hour['feelslike_c'] = (hour['feelslike_f'] - 32) * 5 / 9
+
+    return data
 
 @app.route('/')
 def home():
@@ -24,8 +36,9 @@ def today_hourly():
     """
     Get today's weather with hourly data for a specified zip code.
     """
-    zip_code = request.args.get('zip', '44113')  # Defaulted to display the zip code of 44113 if user does not provide one
-    data = fetch_weather_data(zip_code, 1)
+    zip_code = request.args.get('zip', '44113')
+    temp_unit = request.args.get('temp_unit', 'F')
+    data = fetch_weather_data(zip_code, 1, temp_unit)
     return jsonify(data)
 
 @app.route('/weather/3-day', methods=['GET'])
@@ -33,8 +46,9 @@ def three_day_forecast():
     """
     Get a 3-day weather forecast for a specified zip code.
     """
-    zip_code = request.args.get('zip', '44113')  # Defaulted to display the zip code of 44113 if user does not provide one
-    data = fetch_weather_data(zip_code, 3)
+    zip_code = request.args.get('zip', '44113')
+    temp_unit = request.args.get('temp_unit', 'F')
+    data = fetch_weather_data(zip_code, 3, temp_unit)
     return jsonify(data)
 
 if __name__ == '__main__':
