d609af51bbb1a9bfa446ceca9f7b2704136915fb
Faith <faithmancuso@Faiths-MBP.lan>
2024-12-04 12:47:56 -0500

    removed all sms test features, going to start from scratch to try different sms API's since twilio has to confirm us to use a toll free number, which can take 3-5 days which we do not have that long. In this code is previosu code from a previosu commit, but i readded the functionality for the subscribe button along with a notification that lets the user know when they have subscribed at the top. I added the checkbox that the user must check in order to subscribe, this give us their permission to send them texts.

--- app/main.py
+++ app/main.py
@@ -1,32 +1,14 @@
 from flask import Flask, jsonify, request, render_template
 import requests
 from flask_cors import CORS
-from twilio.rest import Client
-from apscheduler.schedulers.background import BackgroundScheduler
-from dotenv import load_dotenv
-import os
-import datetime
 
 app = Flask(__name__)
+# Explicitly allow requests from our live domain
 CORS(app, resources={r"/*": {"origins": "https://www.forecast-fit.com"}})
 WEATHER_API_KEY = "3fc72f97a7404f9a8d0213532241211"
 
-load_dotenv()
-TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
-
-client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 subscriptions = []
-
-def send_sms(to, body):
-    message = client.messages.create(
-        body=body,
-        from_=TWILIO_PHONE_NUMBER,
-        to=to
-    )
-    return message.sid
 
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
@@ -34,9 +16,10 @@ def subscribe():
     phone = data.get('phone')
     time = data.get('time')
     zip_code = data.get('zip')
+    consent = data.get('consent')
 
-    if not phone or not time or not zip_code:
-        return jsonify({"error": "All fields are required."}), 400
+    if not phone or not time or not zip_code or not consent:
+        return jsonify({"error": "All fields are required, and consent must be provided."}), 400
 
     subscriptions.append({
         "phone": phone,
@@ -46,17 +29,17 @@ def subscribe():
 
     return jsonify({"message": "Subscription successful!"}), 200
 
-def fetch_weather(zip_code):
-    url = f'http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={zip_code}'
-    response = requests.get(url)
-    data = response.json()
-    return data['forecast']['forecastday'][0]['day']['condition']['text']
+
 
 def fetch_weather_data(zip_code, days, temp_unit='F'):
+    """
+    Fetch weather data for a specified number of days.
+    """
     url = f"http://api.weatherapi.com/v1/forecast.json?key={WEATHER_API_KEY}&q={zip_code}&days={days}"
     response = requests.get(url)
     data = response.json()
 
+    # Convert temperatures if necessary
     if temp_unit == 'C':
         for day in data['forecast']['forecastday']:
             day['day']['maxtemp_c'] = (day['day']['maxtemp_f'] - 32) * 5 / 9
@@ -68,24 +51,18 @@ def fetch_weather_data(zip_code, days, temp_unit='F'):
 
     return data
 
-def send_daily_notifications():
-    current_time = datetime.datetime.now().strftime('%H:%M')
-    for subscription in subscriptions:
-        if subscription['time'] == current_time:
-            weather = fetch_weather(subscription['zip'])
-            message = f"Good morning! Here’s your daily weather update for zip code {subscription['zip']}: {weather}."
-            send_sms(subscription['phone'], message)
-
-scheduler = BackgroundScheduler()
-scheduler.add_job(send_daily_notifications, 'interval', minutes=1)
-scheduler.start()
-
 @app.route('/')
 def home():
+    """
+    Serve the homepage.
+    """
     return render_template('index.html')
 
 @app.route('/weather/today-hourly', methods=['GET'])
 def today_hourly():
+    """
+    Get today's weather with hourly data for a specified zip code.
+    """
     zip_code = request.args.get('zip', '44113')
     temp_unit = request.args.get('temp_unit', 'F')
     data = fetch_weather_data(zip_code, 1, temp_unit)
@@ -93,25 +70,13 @@ def today_hourly():
 
 @app.route('/weather/3-day', methods=['GET'])
 def three_day_forecast():
+    """
+    Get a 3-day weather forecast for a specified zip code.
+    """
     zip_code = request.args.get('zip', '44113')
     temp_unit = request.args.get('temp_unit', 'F')
     data = fetch_weather_data(zip_code, 3, temp_unit)
     return jsonify(data)
-
-
-# testing sms
-@app.route('/test-sms', methods=['GET'])
-def test_sms():
-    phone = request.args.get('phone')
-    message = "This is a test message."
-    if phone:
-        sid = send_sms(phone, message)
-        return jsonify({"message": f"Test SMS sent with SID: {sid}"}), 200
-    return jsonify({"error": "Phone number is required."}), 400
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
